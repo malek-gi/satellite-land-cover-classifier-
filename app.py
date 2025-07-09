dd287013424072c780f2b60c7bd7eb0b51322890
Malek Abouda <113708636+malek-gi@users.noreply.github.com>
2025-07-09 17:36:56 +0100

    updates app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ import torch.nn as nn
 my_model = resnet50(weights=ResNet50_Weights.DEFAULT)
 my_model.fc = nn.Sequential(
     nn.Dropout(0.5),
-    nn.Linear(resnet_model.fc.in_features, 256),
+    nn.Linear(my_model.fc.in_features, 256),
     nn.ReLU(),
     nn.Dropout(0.3),
     nn.Linear(256, 10)
